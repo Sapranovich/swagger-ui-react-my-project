b90298daa1b2bcab3a51a54acdee183bdf9d364e
Sapranovich Andrey <SapranovichAndrey@yandex.ru>
2022-01-27 23:22:21 +0300

    fix: rework app

---
+++ src/components/SwaggerUI.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SwaggerUi from "swagger-ui";
+import "swagger-ui/dist/swagger-ui.css";
+
+const SwaggerUI = () => {
+  React.useEffect(() => {
+    SwaggerUi({
+      dom_id: "#swaggerContainer",
+      url: "https://raw.githubusercontent.com/Sapranovich/swagger-ui-react-my-project/main/swagger.yml",
+      // presets: [presets.apis]
+    });
+  }, []);
+  return <div id="swaggerContainer" />;
+};
+
+export default SwaggerUI;

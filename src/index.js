b012201c6e92d232722200892fbcfc2925d14440
Sapranovich Andrey <SapranovichAndrey@yandex.ru>
2022-01-27 21:59:10 +0300

    fix: bugs

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 function App() {
+
   return (
     <div className="App">
-      <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+      <SwaggerUI url="https://raw.githubusercontent.com/Sapranovich/swagger-ui-react-my-project/main/swagger.yml" />
     </div>
   );
 }
